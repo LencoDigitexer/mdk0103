05c40eb49bf7b82b748d6d7d0548c8904b61bf79
Илья Евсеенко <addminuser258@gmail.com>
2021-11-12 19:41:29 +0300

    Пробивка чека

--- JustJava/app/src/main/java/com/lencodigitexer/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/lencodigitexer/justjava/MainActivity.java
@@ -23,6 +23,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
     int NumberOfCups = 0; // переменная для количества чашек
+    int CostOfCups = 0; // стоимость всех чашек
+    int PriceOfCup = 5; // цена одной чашки
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,28 +36,32 @@ public class MainActivity extends AppCompatActivity {
      * Прибавляем к переменной и отображаем кол-во
      */
     public void increment(View view){
+        // Сбрасываем кнопку заказа
         Button submitText = (Button) findViewById(R.id.price_button_submit);
         submitText.setText("ЗАКАЗ!");
         NumberOfCups += 1;
+        CostOfCups += PriceOfCup;
         display(NumberOfCups);
-        displayPrice(NumberOfCups * 5); // стоимость чашек
+        displayPrice(CostOfCups); // стоимость чашек
     }
 
     /**
      * Уменьшаем переменную и отображаем кол-во
      */
     public void decrement(View view){
+        // Сбрасываем кнопку заказа
         Button submitText = (Button) findViewById(R.id.price_button_submit);
         submitText.setText("ЗАКАЗ!");
         // если чашек нет, то не уменьшаем
         if(NumberOfCups == 0){
             display(NumberOfCups = 0) ;
-            displayPrice(NumberOfCups * 5); // стоимость чашек
+            displayPrice(CostOfCups = 0); // стоимость чашек
         }
-        else{
+        else{ // иначе, уменьшаем
             NumberOfCups -= 1 ;
+            CostOfCups -= PriceOfCup;
             display(NumberOfCups); }
-            displayPrice(NumberOfCups * 5); // стоимость чашек
+            displayPrice(NumberOfCups * PriceOfCup); // стоимость чашек
     }
 
 
@@ -67,8 +73,12 @@ public class MainActivity extends AppCompatActivity {
         display(NumberOfCups); // отобращаем кол-во чашек
         displayPrice(NumberOfCups * 5); // стоимость чашек
         Button submitText = (Button) findViewById(R.id.price_button_submit);
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView chequeTextView = (TextView) findViewById(R.id.cheque_text_view);
         if(NumberOfCups > 0){
+            chequeTextView.setText("Чек");
             submitText.setText("Спасибо за заказ!");
+            priceTextView.setText("Общее кол-во кофе: " + NumberOfCups + "\nИтоговая стоимость: " + NumberFormat.getCurrencyInstance().format(CostOfCups) + "\nПриятного кофепития!");
         } else {
             submitText.setText("Сначала выберите заказ!");
         }
